2f86a79ff70ab10dacf9c52c144082b37bf9615c
cathaypacific8747 <cathayexpress4@gmail.com>
2021-06-12 13:39:04 +0800

    add images

--- project/auth.py
+++ project/auth.py
@@ -32,6 +32,23 @@ def signup_post():
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+    stdid = request.form.get('stdid')
+    password = request.form.get('password')
+    remember = bool(request.form.get('remember'))
+
+    user = User.query.filter_by(stdid=stdid).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    return redirect(url_for('main.profile'))
+
 @auth.route('/logout')
 def logout():
     return 'Logout'
