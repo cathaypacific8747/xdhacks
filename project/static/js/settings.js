d1810bdedf06a10a6b8f7af8891465dbec86767a
cathaypacific8747 <abrahamcheung2004@gmail.com>
2021-06-30 13:37:31 +0800

    update save settings logic

--- project/static/js/settings.js
+++ project/static/js/settings.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
     .then((result) => {
         var user = new UserSettings(result["data"]);
         user.populate()
+        user.bindEditActions()
+        user.bindSaveActions()
+        // user.bindSaveAction()
     })
     .catch((error) => {
         console.error(error); 
@@ -32,6 +35,7 @@ $(document).ready(function() {
             this.data = data;
         }
 
+        // utilities
         remove_loader(loader) {
             $(`[data-field="${loader}"]`).addClass("hide").removeClass("shimmerBG");
         }
@@ -44,6 +48,25 @@ $(document).ready(function() {
             $(`[data-field="${container}"]`).addClass("hide");
         }
 
+        bindEditAction(section) {
+            $(`[data-edit="${section}"]`).click(() => {
+                $(`[data-section="${section}_display"]`).addClass("hide");
+                $(`[data-edit="${section}"]`).addClass("hide");
+                $(`[data-section="${section}_editor"]`).removeClass("hide");
+                $(`[data-save="${section}"]`).removeClass("hide");
+            })
+        }
+
+        bindSaveAction(section) {
+            $(`[data-save="${section}"]`).click(() => {
+                $(`[data-section="${section}_editor"]`).addClass("hide");
+                $(`[data-save="${section}"]`).addClass("hide");
+                $(`[data-section="${section}_display"]`).removeClass("hide");
+                $(`[data-edit="${section}"]`).removeClass("hide");
+            })
+        }
+
+        // updaters
         update_profile() {
             $('[data-field="profilePic"]').attr("src", `${this.data.profilePic}=s172-c`).parent().removeClass("shimmerBG");
             $('[data-field="name"]').html(this.data.name).removeClass("shimmerBG");
@@ -55,6 +78,16 @@ $(document).ready(function() {
             }
         }
 
+        update_payment_edit() {
+            const paymentInformations = ['cash', 'octopus', 'payme', 'tapngo', 'bankTransfer', 'eCheque', 'alipay', 'wechatPay'];
+            for (let p of paymentInformations) {
+                if (this.data[p]) {
+                    $(`[data-editfield="${p}"]`).prop("checked", true);
+                } else {
+                    $(`[data-editfield="${p}"]`).prop("checked", false);
+                }
+            }
+        }
         update_payment() {
             const paymentInformations = ['cash', 'octopus', 'payme', 'tapngo', 'bankTransfer', 'eCheque', 'alipay', 'wechatPay'];
             let hasPaymentInformation = false;
@@ -62,6 +95,8 @@ $(document).ready(function() {
                 if (this.data[p]) {
                     hasPaymentInformation = true;
                     $(`[data-field="${p}"]`).attr("src", `static/img/payment/${p}.png`);
+                } else {
+                    $(`[data-field="${p}"]`).attr("src", '');
                 }
             }
             
@@ -70,42 +105,79 @@ $(document).ready(function() {
             } else {
                 $(`[data-field="payment_information_loader"]`).html("Unset").removeClass("hide");
             }
+            this.update_payment_edit()
         }
 
+        update_accountType_edit() {
+            $('[data-editfield="buyer"]').prop("checked", this.data.buyer);
+            $('[data-editfield="seller"]').prop("checked", this.data.seller);
+        }
         update_accountType() {
             if (this.data.seller) {
                 $('[data-field="seller_information_container"]').removeClass("hide")
+                $('[data-field="seller"]').removeClass("hide");
             } else {
                 $('[data-field="seller_information_container"]').addClass("hide")
+                $('[data-field="seller"]').addClass("hide");
             }
-            if (this.data.buyer) $('[data-field="buyer"]').removeClass("hide");
-            if (this.data.seller) $('[data-field="seller"]').removeClass("hide");
+            if (this.data.buyer) {
+                $('[data-field="buyer"]').removeClass("hide");
+            } else {
+                $('[data-field="buyer"]').addClass("hide");
+            }
             if (this.data.buyer || this.data.seller) {
                 $(`[data-field="account_type_loader"]`).html("").addClass("hide");
             } else {
                 $(`[data-field="account_type_loader"]`).html("Unset").removeClass("hide");
             }
+            this.update_accountType_edit()
         }
 
+        update_sellerDeliveryMethods_edit() {
+            $('[data-editfield="inSchoolExchange"]').prop("checked", this.data.inSchoolExchange);
+            $('[data-editfield="meetup"]').prop("checked", this.data.meetup);
+            $('[data-editfield="delivery"]').prop("checked", this.data.delivery);
+        }
         update_sellerDeliveryMethods() {
-            if (this.data.inSchoolExchange) $('[data-field="inSchoolExchange"]').removeClass("hide");
-            if (this.data.meetup) $('[data-field="meetup"]').removeClass("hide");
-            if (this.data.delivery) $('[data-field="delivery"]').removeClass("hide");
+            if (this.data.inSchoolExchange) {
+                $('[data-field="inSchoolExchange"]').removeClass("hide");
+            } else {
+                $('[data-field="inSchoolExchange"]').addClass("hide");
+            }
+            if (this.data.meetup) {
+                $('[data-field="meetup"]').removeClass("hide");
+            } else {
+                $('[data-field="meetup"]').addClass("hide");
+            }
+            if (this.data.delivery) {
+                $('[data-field="delivery"]').removeClass("hide");
+            } else {
+                $('[data-field="delivery"]').addClass("hide");
+            }
+
             if (this.data.inSchoolExchange || this.data.meetup || this.data.delivery) {
                 $(`[data-field="seller_delivery_methods_loader"]`).html("").addClass("hide");
             } else {
                 $(`[data-field="seller_delivery_methods_loader"]`).html("Unset").removeClass("hide");
             }
+            this.update_sellerDeliveryMethods_edit()
         }
 
+        update_sellerNegotiable_edit() {
+            $('[data-editfield="negotiable"]').prop("checked", this.data.negotiable);
+        }
         update_sellerNegotiable() {
             if (this.data.negotiable) {
                 $('[data-field="negotiable"]').removeClass("not-negotiable").addClass("negotiable").html("check");
             } else {
                 $('[data-field="negotiable"]').removeClass("negotiable").addClass("not-negotiable").html("close");
             }
+            this.update_sellerNegotiable_edit()
         }
 
+        update_publicity_edit() {
+            $('[data-editfield="public"]').prop("checked", this.data.public);
+        }
         update_publicity() {
             if (this.data.public) {
                 $('[data-field="public"]').html("Public");
@@ -115,8 +187,13 @@ $(document).ready(function() {
                 $('[data-field="public_icon"]').html("lock").attr("data-tooltip", "Only you can see the information.");
             }
             this.unhide_container("public_container");
+            this.update_publicity_edit();
         }
 
+        update_discord_edit() {
+            $('[data-editfield="discord"]').prop("checked", Boolean(this.data.discord));
+            // add input field
+        }
         update_discord() {
             if (this.data.discord) {
                 $('[data-field="discord_icon"]').attr("src", "static/img/contact/discord.png");
@@ -125,8 +202,13 @@ $(document).ready(function() {
             } else {
                 this.hide_container("discord_container");
             }
+            this.update_discord_edit()
         }
 
+        update_instagram_edit() {
+            $('[data-editfield="instagram"]').prop("checked", Boolean(this.data.instagram));
+            // add input field
+        }
         update_instagram() {
             if (this.data.instagram) {
                 $('[data-field="instagram_icon"]').attr("src", "static/img/contact/instagram.png");
@@ -135,8 +217,14 @@ $(document).ready(function() {
             } else {
                 this.hide_container("instagram_container");
             }
+            this.update_instagram_edit()
         }
 
+        update_phone_edit() {
+            $('[data-editfield="whatsapp"]').prop("checked", this.data.whatsapp);
+            $('[data-editfield="signal"]').prop("checked", this.data.signal);
+            $('[data-editfield="telegram"]').prop("checked", this.data.telegram);
+        }
         update_phone() {
             if (this.data.phone) {
                 $('[data-field="phone_icon"]').attr("src", "static/img/contact/phone.png");
@@ -148,6 +236,7 @@ $(document).ready(function() {
             } else {
                 this.hide_container("phone_container");
             }
+            this.update_phone_edit()
         }
 
         update_customContactInfo() {
@@ -161,14 +250,18 @@ $(document).ready(function() {
         }
 
         populate() {
-            // this.data.seller = true;
-            // this.data.inSchoolExchange = true;
-            // this.data.public = false;
-            // this.data.discord = "cathayexpress#2424";
-            // this.data.instagram = "walter.stop.bullying.me";
-            // this.data.phone = "54102041"
-            // this.data.whatsapp = true;
-            // this.data.customContactInfo = "This is the service hotline for bullying Abraham. Operating hours - 24/7/365. Quality guaranteed, with life-time warranty."
+            this.data.cash = true;
+            this.data.octopus = true;
+            this.data.payme = true;
+            this.data.seller = true;
+            this.data.inSchoolExchange = true;
+            this.data.public = false;
+            this.data.discord = "cathayexpress#2424";
+            this.data.instagram = "walter.stop.bullying.me";
+            this.data.phone = "54102041"
+            this.data.whatsapp = true;
+            this.data.customContactInfo = "This is the service hotline for bullying Abraham. Operating hours - 24/7/365. Quality guaranteed, with life-time warranty."
+            
             this.update_profile();
 
             this.remove_loader("payment_information_loader");
@@ -183,12 +276,29 @@ $(document).ready(function() {
             this.remove_loader("seller_negotiable_loader");
             this.update_sellerNegotiable();
 
-            this.remove_loader("information_loader");
+            this.remove_loader("contact_information_loader");
             this.update_publicity();
             this.update_discord();
             this.update_instagram();
             this.update_phone();
             this.update_customContactInfo();
+        }
+
+        bindEditActions() {
+            this.bindEditAction('payment_information');
+            this.bindEditAction('account_type');
+            this.bindEditAction('seller_delivery_methods');
+            this.bindEditAction('seller_negotiable');
+            this.bindEditAction('contact_information');
+        }
+
+        bindSaveActions() {
+            // add pre-save check
+            this.bindSaveAction('payment_information');
+            this.bindSaveAction('account_type');
+            this.bindSaveAction('seller_delivery_methods');
+            this.bindSaveAction('seller_negotiable');
+            this.bindSaveAction('contact_information');
         }
 
         // setTimeout(() => {
